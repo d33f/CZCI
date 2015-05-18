7cbe0785b8aa9e16fffd1bb841a5b3aec33af952
orelinde <richardje@live.no>
2015-05-18 09:54:25 +0200

    re commit / sync missing files

---
+++ ChronoZoom/ChronoZoom/ChronoZoom.Backend/Data/OrientDb/Factory/TimelineFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChronoZoom.Backend.Data.OrientDb.Factory
+{
+    public class TimelineFactory
+    {
+        public static Entities.Timeline Create(Timeline timeline)
+        {
+            var id = timeline.ORID.RID.Substring(1, timeline.ORID.RID.Length-1);
+            return new Entities.Timeline()
+            {
+                BeginDate = timeline.BeginDate,
+                EndDate = timeline.EndDate,
+                ContentItems = new Entities.ContentItem[0],
+                Id = id,
+                Title = timeline.Title
+            };
+        }
+    }
+}
