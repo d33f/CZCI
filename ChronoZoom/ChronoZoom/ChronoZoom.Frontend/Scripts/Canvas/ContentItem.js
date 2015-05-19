51f1b5a9e481853351c8de094eb27369b1c28946
anthonyhuebers <anthonyhuebers@gmail.com>
2015-05-19 15:30:19 +0200

    styles added for fullscreen content item visualization (not finished)

--- ChronoZoom/ChronoZoom/ChronoZoom.Frontend/Scripts/Canvas/ContentItem.js
+++ ChronoZoom/ChronoZoom/ChronoZoom.Frontend/Scripts/Canvas/ContentItem.js
@@ -183,7 +183,7 @@
 
             updateContainer();
 
-            _container.innerHTML = "test";
+            _container.innerHTML = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.";
 
         } else {
             updateYPosition(contentItems);
@@ -201,9 +201,13 @@
         if (!_hasChildren && _container !== undefined) {
             _container.style.top = _y + "px";
             _container.style.left = _x + "px";
-            //_container.style.width = (_radius * 2) + "px";
-            //_container.style.height = (_radius * 2) + "px";
-            //_container.style.borderRadius = "50%";
+            _container.style.width = (_radius * 2) + "px";
+            _container.style.height = (_radius * 2) + "px";
+            _container.style.borderRadius = "50%";
+            _container.style.display = _isFullScreen ? "block" : "none";
+            _container.style.color = "white";
+
+            //_container.style.pointerEvents = "none";
         }
     }
 
