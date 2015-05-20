bca375625df32ecc9527ac7c9918b5e0499f7ff0
BobGeurts <bobgeurts11@live.nl>
2015-05-20 12:53:58 +0200

    Ultieme swag spacing tussen contentItems

--- ChronoZoom/ChronoZoom/ChronoZoom.Frontend/Scripts/Canvas/ContentItem.js
+++ ChronoZoom/ChronoZoom/ChronoZoom.Frontend/Scripts/Canvas/ContentItem.js
@@ -186,6 +186,26 @@
             _container.innerHTML = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.";
 
         } else {
+            if (_parentContentItem !== undefined) {
+                var positionParent = _parentContentItem.getPosition();
+                var sizeParent = _parentContentItem.getSize();
+                var spacing = sizeParent.width / 40;
+
+                if (positionParent.x != 0 && positionParent.y != 0) {
+
+                    _x >= positionParent.x + spacing ? _x = _x : _x = positionParent.x + spacing;
+
+                    var rightX = _x + _width;
+                    var parentRightX = positionParent.x + sizeParent.width;
+                    var deltaRight = parentRightX - rightX;
+
+                    if (deltaRight < 0)
+                        _width += deltaRight;
+
+                    parentRightX == 0 || rightX <= parentRightX - spacing ? _width = _width : _width -= spacing;
+                }
+            }
+
             updateYPosition(contentItems);
 
             _height = 100;
