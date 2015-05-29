a6b385f3dc23012ffe92d0ff6b58aad00b75ec0f
felixn11 <felixn@live.nl>
2015-05-29 13:25:43 +0200

    Changed unit test for Post/Put

--- ChronoZoom/ChronoZoom/ChronoZoom.Backend/Controllers/TimelineController.cs
+++ ChronoZoom/ChronoZoom/ChronoZoom.Backend/Controllers/TimelineController.cs
@@ -47,9 +47,15 @@ namespace ChronoZoom.Backend.Controllers
             {
                 return BadRequest("timeline invalid");
             }
+            if (timeline.Id == 0)
+            {
+                return BadRequest("No id specified");
+            }
+
             try
             {
-                return Ok(_service.Add(timeline));
+                _service.Update(timeline);
+                return Ok();
             }
             catch (Exception)
             {
@@ -69,15 +75,9 @@ namespace ChronoZoom.Backend.Controllers
             {
                 return BadRequest("timeline invalid");
             }
-            if (timeline.Id == 0)
-            {
-                return BadRequest("No id specified");
-            }
-
             try
             {
-                _service.Update(timeline);
-                return Ok();
+                return Ok(_service.Add(timeline));
             }
             catch (Exception)
             {
