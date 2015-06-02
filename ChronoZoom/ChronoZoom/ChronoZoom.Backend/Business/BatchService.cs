29a32ca4d6de5ced20530ad9651ff2665938abf6
anthonyhuebers <anthonyhuebers@gmail.com>
2015-06-02 10:08:56 +0200

    Tussentijdse checkin

--- ChronoZoom/ChronoZoom/ChronoZoom.Backend/Business/BatchService.cs
+++ ChronoZoom/ChronoZoom/ChronoZoom.Backend/Business/BatchService.cs
@@ -1,9 +1,13 @@
 ﻿using ChronoZoom.Backend.Business.Interfaces;
+using ChronoZoom.Backend.Entities;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace ChronoZoom.Backend.Business
@@ -16,16 +20,54 @@ namespace ChronoZoom.Backend.Business
             {
                 using (StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8, true, 128)) 
                 {
-                    string line = streamReader.ReadLine(); // the timeline
+                    string line = streamReader.ReadLine();
+                    Timeline timeline = CreateTimeline(line);
 
+                    List<Task> tasks = new List<Task>();
+                    ConcurrentBag<ContentItem> contentItems = new ConcurrentBag<ContentItem>();
                     while ((line = streamReader.ReadLine()) != null)
                     {
-                        // all the content items
+                        tasks.Add(Task.Factory.StartNew(() => contentItems.Add(CreateContentItem(line))));
                     }
 
+                    Task.WaitAll(tasks.ToArray());
+
+                    var x = timeline;
+                    var y = contentItems;
                     return -1;
                 }
             }
+        }
+
+        private Timeline CreateTimeline(string s)
+        {
+            JObject json = JObject.Parse(s);
+
+            return new Timeline()
+            {
+                BeginDate = (decimal)json["BeginDate"],
+                EndDate = (decimal)json["EndDate"],
+                Title = (string)json["Title"]
+            };
+        }
+
+        private ContentItem CreateContentItem(string s)
+        {
+            JObject json = JObject.Parse(s);
+
+            return new ContentItem()
+            {
+                Id = (long)json["Id"],
+                ParentId = (long)json["ParentId"],
+
+                BeginDate = (decimal)json["BeginDate"],
+                EndDate = (decimal)json["EndDate"],
+                Title = (string)json["Title"],
+                HasChildren = (bool)json["HasChildren"],
+                PictureURL = (string)json["PictureURL"],
+                SourceURL = (string)json["SourceURL"],
+                SourceRef = (string)json["SourceRef"]
+            };
         }
     }
 }
