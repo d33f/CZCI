4178361c70e73594d62ffa6314825d3adcb435c3
orelinde <richardje@live.no>
2015-06-11 18:22:58 +0200

    Update BatchController.cs

--- ChronoZoom/ChronoZoom/ChronoZoom.Backend/Controllers/BatchController.cs
+++ ChronoZoom/ChronoZoom/ChronoZoom.Backend/Controllers/BatchController.cs
@@ -13,7 +13,7 @@ using System.Threading.Tasks;
 
 namespace ChronoZoom.Backend.Controllers
 {
-    [EnableCors(origins: "http://localhost:20000", headers: "*", methods: "*")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class BatchController : ApiController
     {
         private readonly string _serverUploadFolder = Path.GetTempPath();
