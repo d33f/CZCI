f2b06b0c6c7a284bc77325def8791ee3e7bc0908
orelinde <richardje@live.no>
2015-06-27 14:31:14 +0200

    back

--- ChronoZoom/ChronoZoom/ChronoZoom.Backend/Global.asax.cs
+++ ChronoZoom/ChronoZoom/ChronoZoom.Backend/Global.asax.cs
@@ -18,7 +18,7 @@ namespace ChronoZoom.Backend
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
 
-            GlobalConfiguration.Configuration.Services.Replace(typeof(IHostBufferPolicySelector), new NoBufferPolicySelector());
+            //GlobalConfiguration.Configuration.Services.Replace(typeof(IHostBufferPolicySelector), new NoBufferPolicySelector());
 
             GlobalConfiguration.Configuration.Formatters.Clear();
             GlobalConfiguration.Configuration.Formatters.Add(new JsonMediaTypeFormatter());
@@ -27,15 +27,15 @@ namespace ChronoZoom.Backend
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
 
-            if (Context.Request.Path.Contains("api/") && Context.Request.HttpMethod == "OPTIONS")
-            {
-                Context.Response.AddHeader("Access-Control-Allow-Origin", Context.Request.Headers["Origin"]);
-                Context.Response.AddHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-                Context.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST PUT, DELETE, OPTIONS");
-                // This line is added because whatever is coming from the Register " { SupportsCredentials = true };" is ignored.
-                Context.Response.AddHeader("Access-Control-Allow-Credentials", "true");
-                Context.Response.End();
-            }
+            //if (Context.Request.Path.Contains("api/") && Context.Request.HttpMethod == "OPTIONS")
+            //{
+            //    Context.Response.AddHeader("Access-Control-Allow-Origin", Context.Request.Headers["Origin"]);
+            //    Context.Response.AddHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            //    Context.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST PUT, DELETE, OPTIONS");
+            //    // This line is added because whatever is coming from the Register " { SupportsCredentials = true };" is ignored.
+            //    Context.Response.AddHeader("Access-Control-Allow-Credentials", "true");
+            //    Context.Response.End();
+            //}
 
         } 
     }
